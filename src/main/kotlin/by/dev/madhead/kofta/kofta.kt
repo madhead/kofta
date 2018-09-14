4342eca4f77c818179134204eca81b1472a01059
madhead <siarhei.krukau@gmail.com>
2018-09-14 18:37:51 +0300

    Hello, netdata!

--- src/main/kotlin/by/dev/madhead/kofta/kofta.kt
+++ src/main/kotlin/by/dev/madhead/kofta/kofta.kt
@@ -2,6 +2,23 @@
 
 package by.dev.madhead.kofta
 
+import java.util.Random
+import java.util.concurrent.TimeUnit
+
 fun main(args: Array<String>) {
-    println("Hello, world")
+    val period = args[0].toLong();
+
+    println("""
+        CHART kafka.lag Name Title Unit Family Context line 57005
+        DIMENSION messages "Messages dimension name" absolute 1 1
+    """.trimIndent())
+
+    while (true) {
+        println("""
+            BEGIN kafka.lag
+            SET messages = ${Random().nextInt(100)}
+            END
+        """.trimIndent())
+        Thread.sleep(TimeUnit.SECONDS.toMillis(period));
+    }
 }
