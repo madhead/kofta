81e90e7d064f130b2c4755cbd61c9de13338f8f3
madhead <siarhei.krukau@gmail.com>
2018-09-14 16:40:37 +0300

    Kotlin init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,29 @@
-task<Wrapper>("wrapper") {
-    gradleVersion = "4.10.1"
-    distributionType = Wrapper.DistributionType.ALL
+plugins {
+    kotlin("jvm").version("1.2.70")
+    application
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8", "1.2.70"))
+}
+
+application {
+    mainClassName = "by.dev.madhead.kofta.Kofta"
+}
+
+tasks {
+    val wrapper by creating(Wrapper::class) {
+        gradleVersion = "4.10.1"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+
+    withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).all {
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+    }
 }
---
+++ src/main/kotlin/by/dev/madhead/kofta/kofta.kt
@@ -0,0 +1,7 @@
+@file:JvmName("Kofta")
+
+package by.dev.madhead.kofta
+
+fun main(args: Array<String>) {
+    println("Hello, world")
+}
