f4d0f31888eae44321151f8d4664064bc87b7877
madhead <siarhei.krukau@gmail.com>
2018-09-15 00:00:13 +0300

    Stub implementation for consumers by topic metrics

--- src/main/kotlin/by/dev/madhead/kofta/kofta.kt
+++ src/main/kotlin/by/dev/madhead/kofta/kofta.kt
@@ -2,23 +2,38 @@
 
 package by.dev.madhead.kofta
 
+import java.io.File
 import java.util.Random
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
+const val PRIORITY_BASE = 0xDEAD
+
 fun main(args: Array<String>) {
-    val period = args[0].toLong();
+    val configuration = Configuration.read(File(System.getenv("NETDATA_CONFIG_DIR")!!, "kofta.yml"))
 
-    println("""
-        CHART kafka.lag Name Title Unit Family Context line 57005
-        DIMENSION messages "Messages dimension name" absolute 1 1
-    """.trimIndent())
+    configuration.servers.forEachIndexed { index, server ->
+        println("CHART 'Kafka.consumers_by_topic_${server.name}' '${server.name}' 'Consumers by topic for ${server.name}' 'consumers' '${server.name}' 'consumers_by_topic' line ${PRIORITY_BASE + index} ${server.refreshInterval}")
+        server.topics.forEach {
+            println("DIMENSION 'consumers_by_topic_$it' '$it' absolute")
+        }
+    }
 
-    while (true) {
-        println("""
-            BEGIN kafka.lag
-            SET messages = ${Random().nextInt(100)}
-            END
-        """.trimIndent())
-        Thread.sleep(TimeUnit.SECONDS.toMillis(period));
+    val executor = Executors.newScheduledThreadPool(1) { runnable ->
+        Thread(runnable, "kofta")
+    }
+
+    configuration.servers.forEachIndexed { index, server ->
+        executor.scheduleAtFixedRate({
+            synchronized(System.out) {
+                println("BEGIN 'Kafka.consumers_by_topic_${server.name}'")
+                server.topics.forEach {
+                    println("""
+                        SET 'consumers_by_topic_$it' = ${Random().nextInt(1000)}
+                    """.trimIndent())
+                }
+                println("END")
+            }
+        }, 0, server.refreshInterval, TimeUnit.SECONDS)
     }
 }
